20232b963fc6c87fb38e8666674c01ed8210bf80
arpitgupta1906 <gupta.25.iitj.ac.in>
2020-02-11 00:03:09 +0530

    added image rotate functionality

--- textdetector.py
+++ textdetector.py
@@ -1,20 +1,27 @@
 import cv2
 import pytesseract
 from pytesseract import Output
+from PIL import Image
 
-
-image_path='image5.png'
-
-
+image_path='1.png'
 img=cv2.imread(image_path)
+
+
+
+
+# img = cv2.transpose(img)
+# img=cv2.flip(img,flipCode=1)
+# cv2.imwrite("rotated.jpg",img)
+
+
 
 custom_config=r'--oem 3 --psm 6'
 
-custom_config1=r'--oem 3 --psm 3'
+custom_config1=r'--oem 3 --psm 1'
 
 custom_config2=r'--oem 3 --psm 11'
 
-text=pytesseract.image_to_string(img,config=custom_config2)
+text=pytesseract.image_to_string(img,config=custom_config1)
 d=pytesseract.image_to_data(img,config=custom_config,output_type=Output.DICT)
 
 print(text)
@@ -25,7 +32,8 @@ print(text)
 #         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
 #         img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+
+
+
 # cv2.imshow('img', img)
 # cv2.waitKey(0)
-
-
