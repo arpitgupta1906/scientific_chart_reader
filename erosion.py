0449a6a997ec24db89305818d33fb92902cf515e
raghavranjan005 <ranjan.1@iitj.ac.in>
2020-02-29 01:04:05 -0800

    Added skeletonization erosion and matrix code

---
+++ erosion.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('1.png', 0)
+
+ret,img = cv2.threshold(img, 180, 255, 0)
+
+kernel = np.ones((2,2), np.uint8)
+
+img = cv2.erode(img, kernel, iterations=1)
+#img_dilation = cv2.dilate(img, kernel, iterations=1)
+
+cv2.imshow('Input', img)
+#cv2.imshow('Erosion', img)
+#cv2.imshow('Dilation', img_dilation)
+
+
+#SKELETONIZATION
+
+cv2.waitKey(0)
+
+size = np.size(img)
+skel = np.zeros(img.shape, np.uint8)
+
+# Get a Cross Shaped Kernel
+element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+
+
+# Repeat steps 2-4
+while True:
+    #Step 2: Open the image
+    open = cv2.morphologyEx(img, cv2.MORPH_OPEN, element)
+    #Step 3: Substract open from the original image
+    temp = cv2.subtract(img, open)
+    #Step 4: Erode the original image and refine the skeleton
+    eroded = cv2.erode(img, element)
+    skel = cv2.bitwise_or(skel,temp)
+    img = eroded.copy()
+    # Step 5: If there are no white pixels left ie.. the image has been completely eroded, quit the loop
+    if cv2.countNonZero(img)==0:
+        break
+
+# Displaying the final skeleton
+cv2.imshow("Skeleton",skel)
+#cv2.imwrite('skeletonoftest.png',skel)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ matrix.py
@@ -0,0 +1,4 @@
+import cv2 #opencv
+
+array_value=cv2.imread("test.png")
+print(array_value)
