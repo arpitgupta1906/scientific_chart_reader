ca7503437772b5e89fea23a534ff6fc3465a9e30
arpitgupta1906 <gupta.25.iitj.ac.in>
2020-03-18 00:01:16 +0530

    brought the project together

---
+++ maincode/extracted_data.py
@@ -0,0 +1,83 @@
+import textdetector
+# import json
+
+
+def extractdata(image_path):
+    text1,text2=textdetector.graphtextdetextor(image_path)
+    data1=text1.strip().split('\n')
+    data2=text2.strip().split('\n')
+
+    y_axis_title=data2[0]
+    y_axis_list=[]
+    k=[]
+
+    for i in data1:
+        if i.isdigit():
+            y_axis_list.append(i)
+        else:
+            k.append(i)
+
+    x_axis_title=k[-1]
+    k=k[1:-1]
+
+    graph_title=data1[0]
+    data_titles=k[-1].strip().split(' ')
+
+    diff=[]
+
+    for i in range(len(y_axis_list)-1):
+        diff.append(int(y_axis_list[i])-int(y_axis_list[i+1]))
+
+    freq={}
+    for i in diff:
+        freq[i]=diff.count(i)
+
+    max=0
+    maxnumber=0
+
+    for key,value in freq.items():
+        if value>max:
+            max=value
+            maxnumber=key
+
+    ydata=[]
+    i=0
+    while True:
+        o=int(y_axis_list[0])-i*maxnumber
+        if o>0:
+            ydata.append(o)
+        else:
+            ydata.append(0)
+        if o<=0:
+            break
+        i=i+1
+    if graph_title==str(ydata[0]):
+        graph_title=""
+
+    data_dictionary={
+        "Graph_title":graph_title,
+        "Yaxis_title":y_axis_title,
+        "Xaxis_title":x_axis_title,
+        "Yaxis_plotdata":ydata,
+        "Data_titles":data_titles
+    }
+
+    # print("Graph Title: ",graph_title)
+    # print("Y Axis title: ",y_axis_title)
+    # print("X Axis title: ",x_axis_title)
+    # print("Y axis plot list: ",ydata)
+    # print("Data titles: ",data_titles)
+
+    # print(data_dictionary)
+    return [data_dictionary,maxnumber]
+
+    # with open('person.txt','w') as json_file:
+    # json.dump(data_dictionary,json_file)
+
+
+
+
+if __name__=='__main__':
+    image_path='1.png'
+    a=extractdata(image_path)
+    print(a)
---
+++ maincode/mainscript.py
@@ -0,0 +1,4 @@
+import cv2
+import bar_extracter
+import heightcalculator
+import extracted_data
