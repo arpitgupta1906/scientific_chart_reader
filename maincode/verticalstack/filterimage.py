5ac150f6c92fad341b693496875c0af84390546f
arpitgupta1906 <gupta.25.iitj.ac.in>
2020-06-21 09:54:10 +0530

    prepared the filters for horizontal graphs

--- maincode/verticalstack/filterimage.py
+++ maincode/verticalstack/filterimage.py
@@ -11,10 +11,10 @@ def obtainbars(img):
 
 
     # Thresholding the image
-    (thresh, img_bin) = cv2.threshold(img, 240, 255,0)
+    # (thresh, img_bin) = cv2.threshold(img, 240, 255,0)
     # Invert the image
     
-    # img_bin=img
+    img_bin=img
     # img_bin=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
     img_bin = 255-img_bin 
@@ -23,26 +23,28 @@ def obtainbars(img):
     # plt.show()
 
     # Defining a kernel length
-    kernel_length = np.array(img).shape[1]//100
+    kernel_length = np.array(img).shape[1]//80
     
-    # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
-    hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 10))
+    # A vertical kernel of (1 X kernel_length), which will help to detect all the vertical line from the image.
+    verticle_kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(10,kernel_length))
+
 
     # A kernel of (3 X 3) ones.
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
+    # Morphological operation to detect vertical lines from an image
+    
+    img_temp1=cv2.erode(img_bin,verticle_kernel,iterations=3)
+    verticle_lines_img=cv2.dilate(img_temp1,verticle_kernel,iterations=3)
+    verticle_lines_img=255-verticle_lines_img
 
-    # Morphological operation to detect horizontal lines from an image
-    img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
-    horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
-    horizontal_lines_img = 255-horizontal_lines_img
+    return verticle_lines_img
 
-    return horizontal_lines_img
     
 if __name__=="__main__":
     # Read the image
-    # img = cv2.imread('1.png', 0)
-    img=cv2.imread('test2.jpg',0)
+    img = cv2.imread('case4.png', 0)
+    # img=cv2.imread('mixed.jpg',0)
     horizontal_lines_img=obtainbars(img)
     # cv2.imwrite("test_filtered.png",horizontal_lines_img)
     plt.imshow(horizontal_lines_img,cmap="gray")
