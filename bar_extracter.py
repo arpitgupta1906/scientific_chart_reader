fcdee1b6a24ac1ef87014e276f6d11d24183c7a2
arpitgupta1906 <gupta.25@iitj.ac.in>
2021-01-13 00:35:24 +0530

    removed presentation file

--- bar_extracter.py
+++ bar_extracter.py
@@ -35,7 +35,7 @@ def obtainbars(img):
 if __name__=="__main__":
     # Read the image
     # img = cv2.imread('1.png', 0)
-    img=cv2.imread('please3.png',0)
+    img=cv2.imread('test_image.png',0)
     horizontal_lines_img=obtainbars(img)
     # cv2.imwrite("please_test.png",horizontal_lines_img)
     plt.imshow(horizontal_lines_img,cmap="gray")
